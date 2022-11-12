c57beb6247b6ff4d564380ac6f4f7c5dfa1dda1f
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-11-12 14:54:33 +0100

    Changed date format

--- public/Script/getButton.js
+++ public/Script/getButton.js
@@ -4,12 +4,15 @@ const baseUrl = 'http://localhost:8383/data'
 
 getBtn.addEventListener('click', getInfo);
 async function getInfo(e) {
-  e.preventDefault();
-  var res = await fetch(baseUrl,
-    {
-      method: 'GET'
-    })
-  console.log(res)
-  var data = await res.json();
-  text.innerHTML = "Heartrate: " + data.heartrate + "; Timestamp: " + data.timestamp;
+    e.preventDefault();
+    var res = await fetch(baseUrl,
+        {
+            method: 'GET'
+        })
+    console.log(res)
+    var data = await res.json();
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    data.timestamp = today.toLocaleString();
+    text.innerHTML = "Heartrate: " + data.heartrate + "; Timestamp: " + data.timestamp;
 }
